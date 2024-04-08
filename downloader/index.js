3e62279d01d66e72d8dc3343e4f777a35b04d591
Dino <theanhvu1501@gmail.com>
2024-04-08 11:32:42 +0700

    first commit

---
+++ downloader/index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const ytdl = require('ytdl-core')
+const fs = require('fs')
+const { last } = require('lodash')
+const { default: axios } = require('axios')
+const EventEmitter = require('events')
+// Array of YouTube video URLs
+const videoURLs = ['https://www.youtube.com/watch?v=fr5-33--zPk']
+class Downloader extends EventEmitter {
+  constructor() {
+    super()
+  }
+  async downloadVideo(videoURL) {
+    try {
+      const info = await ytdl.getInfo(videoURL)
+      const title = info.videoDetails.title
+      const formatsWithAudio720p = info.formats.filter(
+        (format) => format.hasAudio && format.hasVideo && format.quality === 'hd720'
+      )
+      if (formatsWithAudio720p.length > 0) {
+        //Download video
+        const format = formatsWithAudio720p[0]
+        const video = ytdl(videoURL, { format: format })
+        video.pipe(fs.createWriteStream(`${title}.mp4`)) // Save video as .mp4 file
+        video.on('end', () => console.log(`Downloaded video with sound: ${title}`)) // Log when download is complete
+
+        //Download thumb
+        const thumbnailURL = last(info.videoDetails.thumbnails).url
+        const response = await axios.get(thumbnailURL, {
+          responseType: 'stream'
+        })
+        const thumbnailFileName = `${title}.jpg`
+        response.data.pipe(fs.createWriteStream(thumbnailFileName)) // Save thumbnail as .jpg file
+        console.log(`Downloaded thumbnail: ${thumbnailFileName}`)
+      } else {
+        console.error(`Video format not found for ${title}`)
+      }
+    } catch (error) {
+      console.error(`Error downloading ${videoURL}: ${error}`)
+    }
+  }
+
+  // Function to download multiple videos
+  async downloadVideos() {
+    await Promise.all(
+      videoURLs.map((v) => {
+        this.downloadVideo(v)
+      })
+    )
+  }
+}
+
+module.exports = Downloader
---
+++ src/renderer/src/components/Calculator.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react'
+const { ipcRenderer } = window.require('electron')
+let allowedValues = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '+',
+  '-',
+  '/',
+  '*',
+  '=',
+  '.',
+  'C',
+  'Enter',
+  'Backspace',
+  'Delete'
+]
+window.ipcRenderer = ipcRenderer
+const Calculator = () => {
+  const [expression, setExpression] = useState('')
+
+  const startDownload = (e) => {
+    e.preventDefault()
+
+    ipcRenderer.send('download')
+  }
+
+  let calculateExpression = (value) => {
+    if (value === 'C') {
+      setExpression('')
+    } else if (value === '=' || value === 'Enter') {
+      setExpression((expression) => `${eval(expression)}`)
+    } else if (value === 'Backspace' || value === 'Delete') {
+      setExpression((expression) => expression.substring(0, expression.length - 1))
+    } else {
+      setExpression((expression) => expression + value)
+    }
+  }
+
+  let handleButtonClick = (e) => {
+    let value = e.target.dataset.value
+    if (value) {
+      calculateExpression(value)
+    }
+  }
+
+  let handleKeyDown = (e) => {
+    let value = e.key
+    if (value === 'Enter') {
+      e.preventDefault()
+    }
+    if (allowedValues.includes(value)) {
+      calculateExpression(value)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  return (
+    <div className="container" onClick={handleButtonClick}>
+      <div className="result-container">
+        <span className="result">{expression}</span>
+      </div>
+      <div className="row">
+        <button data-value="C">C</button>
+        <button data-value="%">%</button>
+        <button data-value="Backspace">&#x021D0;</button>
+        <button data-value="/">/</button>
+      </div>
+      <div className="row">
+        <button data-value="7">7</button>
+        <button data-value="8">8</button>
+        <button data-value="9">9</button>
+        <button data-value="*">*</button>
+      </div>
+      <div className="row">
+        <button data-value="4">4</button>
+        <button data-value="5">5</button>
+        <button data-value="6">6</button>
+        <button data-value="+">+</button>
+      </div>
+      <div className="row">
+        <button data-value="1">1</button>
+        <button data-value="2">2</button>
+        <button data-value="3">3</button>
+        <button data-value="-">-</button>
+      </div>
+      <div className="row">
+        <button data-value="**">**</button>
+        <button data-value="0">0</button>
+        <button data-value=".">.</button>
+        <button data-value="=">=</button>
+      </div>
+      <div className="row">
+        <button onClick={startDownload}> Start download</button>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
