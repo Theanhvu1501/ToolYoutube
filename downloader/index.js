f9c15eef441af05ed0d324a0f157445d504f573a
Dino <theanhvu1501@gmail.com>
2024-05-02 09:34:34 +0700

    update download video < HD720

--- downloader/index.js
+++ downloader/index.js
@@ -52,6 +52,31 @@ class Downloader extends EventEmitter {
     }, this._throttleValue)
   }
 
+  customSort(a, b) {
+    // Extract resolution and fps (if present)
+    const [resA, fpsA] = a?.qualityLabel?.split(' ')
+    const [resB, fpsB] = b?.qualityLabel?.split(' ')
+
+    // Extract numeric resolution values
+    const numA = parseInt(resA)
+    const numB = parseInt(resB)
+
+    // Compare resolution values
+    if (numA !== numB) {
+      return numB - numA // Sort by resolution first
+    }
+
+    // If resolution is the same, compare fps
+    if (fpsA && fpsB) {
+      const fpsNumA = parseInt(fpsA)
+      const fpsNumB = parseInt(fpsB)
+      return fpsNumB - fpsNumA
+    }
+
+    // If one or both do not have fps, sort them as equal
+    return 0
+  }
+
   downloadVideo = async (videoURL, directory) => {
     if (!this.validateURL(videoURL)) return
     try {
@@ -69,15 +94,25 @@ class Downloader extends EventEmitter {
       const info = await ytdl.getInfo(videoURL)
       const { title, lengthSeconds } = info.videoDetails
       const thumbnailURL = last(info.videoDetails.thumbnails).url
+
       const formatsWithAudio720p = info.formats.find(
         (format) => format.hasAudio && format.hasVideo && format.quality === 'hd720'
       )
 
+      const formatsSortedByQuality = info.formats.sort(
+        (a, b) => parseInt(b.qualityLabel) - parseInt(a.qualityLabel)
+      )
+      const highestQualityFormat = formatsSortedByQuality.find(
+        (format) => format.hasAudio && format.hasVideo
+      )
+
+      const selectedFormat = formatsWithAudio720p || highestQualityFormat
+
       const filePathVideo = path.join(`${directory}/Video`, `${title.replace(/[«»?|"']/g, '')}.mp4`)
       const filePathThumb = path.join(`${directory}/Thumb`, `${title.replace(/[«»?|"']/g, '')}.jpg`)
-      if (formatsWithAudio720p) {
+      if (selectedFormat) {
         //Download video
-        const video = ytdl(videoURL, { format: formatsWithAudio720p })
+        const video = ytdl(videoURL, { format: selectedFormat })
         const videoFileStream = fs.createWriteStream(filePathVideo)
         await new Promise((resolve, reject) => {
           video
